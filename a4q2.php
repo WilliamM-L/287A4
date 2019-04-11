9ab4421c260dbc12859ed3fb39147cfbcf2dea80
William <w.morin.laberge@gmail.com>
2019-04-10 20:51:14 -0400

    Done with #2!

--- a4q2.php
+++ a4q2.php
@@ -19,23 +19,26 @@ if (isset($_POST["reset"]) and $_POST["reset"]=="yes"){
     <body>
     <h1>Assignment 4 Question 2</h1>
     <?php
-    if (!isset($_SESSION["name"])) {?>
-    <form action="a4q2.php" method=POST>
-        <label>What is your name? <input name="username" value="Vlad" type="text"><input type="submit" value="Send"></label>
-    </form>
-    <?php}
-    if( !empty($_SESSION["name"])){
+    if (!isset($_SESSION["name"])) { ?>
+        <form action="a4q2.php" method=POST>
+            <label>What is your name? <input name="username" value="Vlad" type="text"><input type="submit" value="Send"></label>
+        </form>
+        <?php
+    }
+    if(!empty($_SESSION["name"])) {
         $_SESSION["visits"]++;
         ?>
-    <form action="a4q2.php" method=POST>
+        <form action="a4q2.php" method=POST>
+            <?php
+            //echo hi username, you've been here x times!
+            echo "<h3>Hello " . $_SESSION["name"] . "!</h3>";
+            echo "<p>You've been here " . $_SESSION["visits"] . " times!</p>"
+            ?>
+            <label><input value="yes" name="reset" type="hidden"></label>
+            <label><input type="submit" value="Reset"></label>
+        </form>
         <?php
-        //echo hi username, you've been here x times!
-        ?>
-        <label><input value="yes" name="reset" type="hidden"></label>
-        <label><input type="submit" value="Reset"></label>
-    </form>
-    <?php}
-
+    }
     ?>
 
     </body>
